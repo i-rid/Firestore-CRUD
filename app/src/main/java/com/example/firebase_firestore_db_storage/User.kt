955bd5e4276ee9435bdd7d4a75c58920dd15fc72
Ridwan Ahamad <62206988+i-rid@users.noreply.github.com>
2023-05-25 15:04:14 +0600

    updated the User.kt here as well

--- app/src/main/java/com/example/firebase_firestore_db_storage/User.kt
+++ app/src/main/java/com/example/firebase_firestore_db_storage/User.kt
@@ -1,7 +1,7 @@
 package com.example.firebase_firestore_db_storage
 
 data class User (
-    val fName:String,
-    val lName:String,
+    val fname:String,
+    val lname:String,
     val age:Int
 )
